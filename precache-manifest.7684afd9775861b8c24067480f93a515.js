b8baa758141b5c1044a8b7af62da8aaecd2286a0
shi-ivam <rockyshivammm67@gmail.com>
2020-07-30 23:36:07 +0530

    :rocket:

---
+++ precache-manifest.7684afd9775861b8c24067480f93a515.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaf7ecf0a9f2137aaa707a8c5348a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d583f5b7908a464c6c8",
+    "url": "/static/css/main.98ab4730.chunk.css"
+  },
+  {
+    "revision": "ea4e6dfcce834d7bbd6b",
+    "url": "/static/js/2.9754d9cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9754d9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d583f5b7908a464c6c8",
+    "url": "/static/js/main.a8f05fa6.chunk.js"
+  },
+  {
+    "revision": "5c912f209589967d0376",
+    "url": "/static/js/runtime-main.120ba229.js"
+  },
+  {
+    "revision": "9c1ba7104b2436c8727769f61996981d",
+    "url": "/static/media/Sora-Medium.9c1ba710.ttf"
+  },
+  {
+    "revision": "aa64200d21f88f287becbe3f3cebaf0e",
+    "url": "/static/media/Sora-Regular.aa64200d.ttf"
+  }
+]);
